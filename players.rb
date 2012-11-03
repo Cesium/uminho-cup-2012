6e3cd519c71be50df2ce6bb8f22146c66bdee41b
Miguel Palhas <mpalhas@gmail.com>
2012-11-03 19:24:35 +0000

    fixed script for players

--- players.rb
+++ players.rb
@@ -12,12 +12,47 @@ def add_player(team, first_name, last_name)
                                   confirmed_at: Time.now
   user.confirm!
   user.save
+  $COUNT = $COUNT + 1
 
-  player = PlayFutsal::Athlete.create user: user,
+  player = PlayFutsal::Athlete.create user_id: user.id,
                                       sequence: team.athletes.count
 end
 
 find_team("Economia") do |team|
   add_player team, "José", "Luís Ferreira"
-  #add_player team, 
+  #add_player team, "Wilson", "Santos"
+  #add_player team, "Tiago", "Rodrigues"
+  #add_player team, "Miguel", "Lopes"
+  #add_player team, "Antonio", "Coelho"
+  #add_player team, "Sérgio", "Gonçalves"
+  #add_player team, "Ulisses", "Soares"
 end
+
+=begin
+find_team("TroLEI FC") do |team|
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+end
+
+find_team("Los Melones") do |team|
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+end
+
+find_team("Rossas FC") do |team|
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+  add_player_team, "", ""
+end
+=end
