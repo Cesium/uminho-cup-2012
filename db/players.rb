880308c83d174c7b751a0960a09cf2b08cec3c7f
Miguel Palhas <mpalhas@gmail.com>
2012-11-03 19:06:27 +0000

    started player script

---
+++ db/players.rb
@@ -0,0 +1,21 @@
+def find_team(name)
+  yield PlayFutsal::Team.find_by_name(name)
+end
+
+$COUNT = 0
+def add_player(team, first_name, last_name)
+  user = PlayFutsal::User.create  email: "dummy#{$COUNT}@uminho.cup",
+                                  first_name: first_name,
+                                  last_name:  last_name,
+                                  confirmed_at: Time.now
+  user.confirm!
+  user.save
+
+  player = PlayFutsal::Athlete.create user: user,
+                                      sequence: team.athletes.count
+end
+
+find_team("Economia") do |team|
+  add_player team, "José", "Luís Ferreira"
+  #add_player team, 
+end
